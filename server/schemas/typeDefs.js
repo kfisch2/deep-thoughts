08df6aef0e7fdd3921c214b1715f676b5d0ba4fe
kfisch2 <kmlindsey24@gmail.com>
2022-09-19 19:45:37 -0700

    graphQL queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,33 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    thoughts: [Thought]
+    friends: [User]
+    friendCount: Int
+  }
   type Query {
-    helloWorld: String
+    thoughts(username: String): [Thought]
+    users: [User]
+    user(username: String!): User
+    thought(_id: ID!): Thought
   }
 `;
 module.exports = typeDefs;
