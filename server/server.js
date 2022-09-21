dcddadc29d1845fa30caf1603bdcb1de7227e568
kfisch2 <kmlindsey24@gmail.com>
2022-09-21 15:05:34 -0700

    thoughts rendering on page

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { authMiddleware } = require('./utils/auth')
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
