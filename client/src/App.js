39db9afd0ad2e9fdb54e675c588ee31ee4e9d376
kfisch2 <kmlindsey24@gmail.com>
2022-10-17 15:09:28 -0700

    added React router

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import { Login, NoMatch, SingleThought, Profile, Signup } from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import {
   ApolloProvider,
@@ -22,13 +24,22 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/thought" element={<SingleThought />} />
+              <Route path="*" element={<NoMatch/>}/>
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import Login from './Login';
+import NoMatch from './NoMatch';
+import SingleThought from './SingleThought';
+import Profile from './Profile';
+import Signup from './Signup';
+
+export { Login, NoMatch, SingleThought, Profile, Signup }
